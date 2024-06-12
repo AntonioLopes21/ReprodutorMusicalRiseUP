4d0af6b8716b5e307e7cf0c713763d9a2abb24f2
ANTONIO DE PÁDUA BARRETO LOPES FILHO <antonio.lopes@a.ficr.edu.br>
2024-06-12 16:44:44 -0300

    tornando a api acessável

---
+++ api.js
@@ -0,0 +1,35 @@
+// api.js
+
+const url = "https://8000-rdrgotxra-apiplayer-0mteohlhcqu.ws-us114.gitpod.io/api/music/";
+
+function fazerRequisicaoAPI() {
+    // Desativa o botão enquanto a requisição está sendo feita
+    apiBtn.disabled = true;
+
+    fetch(url)
+        .then(response => {
+            // Verifica se a resposta está OK (status 200)
+            if (!response.ok) {
+                throw new Error('Erro ao carregar a API');
+            }
+            return response.json();
+        })
+        .then(data => {
+            console.log(data);
+            // Redireciona após o sucesso da solicitação
+            window.location.href = url;
+        })
+        .catch(error => {
+            console.error('Erro ao carregar a API:', error);
+            // Exibe uma mensagem de erro para o usuário
+            alert('Erro ao carregar a API. Por favor, tente novamente mais tarde.');
+        })
+        .finally(() => {
+            // Reativa o botão após a conclusão da requisição (independentemente do resultado)
+            apiBtn.disabled = false;
+        });
+}
+
+// Evento de clique ao botão
+const apiBtn = document.querySelector("#btn-api")
+apiBtn.addEventListener("click", fazerRequisicaoAPI);
